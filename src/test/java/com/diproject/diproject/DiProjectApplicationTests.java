2f5defc960ec08b0cd94f497cdb5f7dbf82825dc
toandh <toandh.thuylinh@gmail.comT>
2023-03-22 19:17:16 +0700

    init test by DI

--- src/test/java/com/diproject/diproject/DiProjectApplicationTests.java
+++ src/test/java/com/diproject/diproject/DiProjectApplicationTests.java
@@ -1,13 +1,34 @@
 package com.diproject.diproject;
 
+import com.diproject.diproject.controlers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DiProjectApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void testAutoWireOfController(){
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx(){
+		MyController myController = applicationContext.getBean(MyController.class);
+		System.out.println(myController.sayHello());
+	}
+
+
 
 }
